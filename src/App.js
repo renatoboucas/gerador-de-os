a2200906b19ba4057851b47fc2c9dd02922e8e11
Renato Boucas <renatoboucas@gmail.com>
2023-01-24 20:26:19 -0500

    app cleaning errors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import {fetchData} from './AwsFunctions';
 import { UpdateRegister } from './components/osregister/UpdateRegister';
 import { NewRegister } from './components/osregister/NewRegister';
 
-
-
 function App() {
-
 
   return (
     <div className="App">
       <NewRegister/>
       {/* <UpdateRegister/> */}
-      <button onClick={() => fetchDataFormDynamoDb()}> Fetch </button>
-      <button onClick={() => handleEnv()}> ENV </button>
     </div>
   );
 }
